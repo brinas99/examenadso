292e9f78a66c759942ca7ef191c1c4b21126e7c6
brinas99 <55139861+brinas99@users.noreply.github.com>
2019-12-19 07:54:56 +0100

    Add files via upload

---
+++ script6.2_2.ps1
@@ -0,0 +1,45 @@
+﻿Write-Output "Introduce 2 numeros para hacer operaciones"
+Write-Output "el primer numero: "
+$uno = Read-Host
+Write-Output "el segundo numero: "
+$dos = Read-Host
+if($uno -eq $dos){
+$resultado = [int]$uno + $dos
+Write-Output "La suma es: "$resultado
+$resultado = [int]$uno - $dos
+Write-Output "La resta es: "$resultado
+$resultado = [int]$uno * $dos
+Write-Output "La multiplicacion es: "$resultado
+if($dos -eq 0){
+Write-Host "No se puede dividir por cero"}
+else{
+$resultado = [int]$uno / $dos
+Write-Output "La división es: "$resultado}
+}
+if($uno -gt $dos){
+$resultado = [int]$uno + $dos
+Write-Output "La suma es: "$resultado
+$resultado = [int]$uno - $dos
+Write-Output "La resta es: "$resultado
+$resultado = [int]$uno * $dos
+Write-Output "La multiplicacion es: "$resultado
+if($dos -eq 0){
+Write-Host "No se puede dividir por cero"}
+else{
+$resultado = [int]$uno / $dos
+Write-Output "La división es: "$resultado}
+}
+if($uno -lt $dos){
+$resultado = [int]$uno + $dos
+Write-Output "La suma es: "$resultado
+$resultado = [int]$dos - $uno
+Write-Output "La resta es: "$resultado
+$resultado = [int]$uno * $dos
+Write-Output "La multiplicacion es: "$resultado
+if($uno -eq 0){
+Write-Host "No se puede dividir por 0"
+}
+else{
+$resultado = [int]$dos / $uno
+Write-Output "La división es: "$resultado}
+}
